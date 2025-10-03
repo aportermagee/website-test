5ededbcf8e44f1e8be2c32f748885b9c5870b5da
aportermagee <aportermagee@gmail.com>
2025-10-03 12:49:58 -0400

    Update login.js

--- login.js
+++ login.js
@@ -1,6 +1,8 @@
-let users = {
+const users = {
   "amagee": "apm2010!";
 }
+
+const submitBtn = document.getElementById("submitBtn");
 
 function check(username, password) {
   if (users.username === password) {
@@ -9,3 +11,5 @@ function check(username, password) {
     alert("Incorrect password");
   }
 }
+
+submitBtn.onclick(check(document.getElementById("username").value, document.getElementById("password").value);
