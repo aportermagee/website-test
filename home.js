e6cf6f7e776c9d6f15a6d33291fe90695741c5fc
aportermagee <aportermagee@gmail.com>
2025-10-04 12:24:33 -0400

    Update home.js

--- home.js
+++ home.js
@@ -1,3 +1,10 @@
 if (!localStorage.getItem('loggedIn')) {
   window.location.href = 'index.html';
 }
+
+const logoutBtn = document.getItemById('logoutBtn');
+
+logoutBtn.onclick = function() {
+  localStorage.removeItem('loggedIn');
+  window.location.href = 'index.html';
+}
